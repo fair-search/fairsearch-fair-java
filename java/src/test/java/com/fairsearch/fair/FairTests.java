c23f27e3634e88612c0817be8ab5139fc00d37ec
Ivan Kitanovski <ivan.kitanovski@gmail.com>
2019-02-18 20:02:24 +0100

    Added  new test for exact pre-computed numbers

--- java/src/test/java/com/fairsearch/fair/FairTests.java
+++ java/src/test/java/com/fairsearch/fair/FairTests.java
@@ -3,6 +3,8 @@ package com.fairsearch.fair;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.util.LuceneTestCase;
 
+import java.util.Arrays;
+
 public class FairTests extends LuceneTestCase {
 
     public void testAdjustAlpha() {
@@ -50,6 +52,20 @@ public class FairTests extends LuceneTestCase {
         double res = fair.computeFailureProbability(adjustedMTable);
 
         assertEquals(true, res > 0 && res < 1);
+    }
+
+
+    public void testComputeFailureProbabilityExactNumbers() {
+        int n = 10;
+        double p = 0.2;
+        double alpha = 0.15;
+        Fair fair = new Fair(n, p, alpha);
+
+        int[] adjustedMTable = fair.createAdjustedMTable();
+
+        double res = fair.computeFailureProbability(adjustedMTable);
+
+        assertEquals(true, res == 0.1342177280000001);
     }
 
 
